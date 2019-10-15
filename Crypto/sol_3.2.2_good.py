7a3d81d159c677268e5cfa7181cb03afae163e5c
yhyuan2 <yhyuan2@illinois.edu>
2019-10-16 05:52:24 +0800

    3.2.2 with problems

---
+++ Crypto/sol_3.2.2_good.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+blob = """
+\00\00\00\00\00\00\00\00\00\00\00T2߇n\B3\C9\E8l\C62\C5\84\DEj\8D\ABa\F0\83X\87}5\92z\FDK\C0\F9p\AD\F4\EA:\EE\EB\EB\E5\EC$\F0\D7nI\C6U\D6*W,\DDx\ACanT\8Dq*XUCd\D42.8\9B\D9sA\84\B7Y\F2\A4\DE/\BF\AC;\86\ED\FF\C7_`k\C1m\8A\C8\EE\F6k\83?Z\A3k\B9\A8\CD\CB\EE\A7!\AB\F5\B2s~[*\BD\A9\85\CE"""
+from hashlib import sha256
+if sha256(blob).hexdigest() == "696fd4eb46ad91f0292d6f19878bfb300626250fb1048f754110b51d0876f18c":
+	print("Prepare to be destroyed!")
+else:
+	print("I come in peace.")
